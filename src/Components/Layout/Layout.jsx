5b5acdad880083d6b0cde52e50932f438db88e9c
OfirIsHere <Ofirdoron19@gmail.com>
2024-12-07 17:29:41 +0200

    initial setup redux

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "src/Redux/actions";
 
 function Layout() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
   return (
-    <div>Layout</div>
-  )
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button
+        onClick={() => dispatch(decrement())}
+        style={{ marginLeft: "10px" }}
+      >
+        Decrement
+      </button>
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Layout from './Components/Layout/Layout';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Layout />
+    <Provider store={store}>
+      <Layout />
+    </Provider>
   </React.StrictMode>
 );
